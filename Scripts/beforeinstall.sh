13b06e9c1951da50825bcc648906f9c2abdbe120
Ambika Ranganathan <ambika.ranganathan@sjrb.ca>
2018-03-09 18:27:34 -0700

    before install

--- Scripts/beforeinstall.sh
+++ Scripts/beforeinstall.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 #Create Application Directory
-mkdir application
-chown ec2-user:ec2-user application
-cd application
-mkdir bestbuy
+if [ -d '/opt/application/bestbuy' ];
+then
+  echo "Application directory exists"
+else
+  cd /opt
+  mkdir application
+  chown ec2-user:ec2-user application
+  cd application
+  mkdir bestbuy
+fi
